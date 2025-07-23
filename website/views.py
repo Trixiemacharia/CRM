8b155ee426b4e51a3806c3350f5979b82b842d62
Trixiemacharia <machariastacy034@gmail.com>
2025-07-23 17:24:03 +0300

    modified:   website/__pycache__/urls.cpython-312.pyc modified:   website/__pycache__/views.cpython-312.pyc new file:   website/forms.py modified:   website/templates/base.html modified:   website/templates/home.html modified:   website/templates/navbar.html new file:   website/templates/register.html modified:   website/urls.py modified:   website/views.py

--- website/views.py
+++ website/views.py
@@ -1,27 +1,33 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout 
 from django.contrib import messages
 
 # Create your views here.
 def home(request):
     #check to see the logging in
     if request.method =='POST':
-        username = request.POST['username']
-        password = request.POST['password']
+        username = request.POST.get('username')
+        password = request.POST.get('password')
     #authenticate
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request,user)
-        messages.success(request,'You have been logged in')
-        return redirect('home')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,'You have been logged in')
+            return redirect('home')
+        else:
+            messages.success(request,'There was an error logging in')
+            return redirect('home')
+        
     else:
-        messages.success(request,'There was an error logging in')
-        return redirect('home')
-    
-    return render(request, 'home.html',{})
+       return render(request, 'home.html', {})
 
 def login_user(request):
     pass
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, 'you have been logged out')
+    return render(request, 'home.html')
+
+def register_user(request):
+    return render(request, 'register.html', {})
